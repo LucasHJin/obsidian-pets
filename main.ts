390e5a7402db2451a785be2decf5ae7464a2daf9
Lucas Jin <kblazer20@gmail.com>
2025-06-07 19:59:23 -0400

    More testing with plugin

--- main.ts
+++ main.ts
@@ -4,7 +4,36 @@
 import { Plugin } from 'obsidian';
 
 export default class ExamplePlugin extends Plugin {
+	statusBarTextElement: HTMLSpanElement;
+
+	private updateLineCount(fileContent?: string) {
+		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
+		const linesWord = count === 1 ? "line" : "lines";
+		this.statusBarTextElement.textContent = `${count} ${linesWord}` 
+	}
+
+	private async enableLineCount() {
+		const file = this.app.workspace.getActiveFile();
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		} else {
+			this.updateLineCount("0");
+		}
+	}
+
 	onload(): Promise<void> | void {
-		console.log("Hello World!")
+		this.statusBarTextElement = this.addStatusBarItem().createSpan();
+		this.enableLineCount();
+
+		this.app.workspace.on('active-leaf-change', async () => {
+			this.enableLineCount();
+		});
+
+		this.app.workspace.on('editor-change', editor => {
+			const content = editor.getDoc().getValue();
+			this.updateLineCount(content);
+
+		});
 	}
 }
