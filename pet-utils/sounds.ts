f87c08eed707b8ebbd825101ebbaeb485f8c24ea
Lucas Jin <kblazer20@gmail.com>
2026-06-21 23:45:42 -0400

    change: add variation to pitch and sound for meow

--- pet-utils/sounds.ts
+++ pet-utils/sounds.ts
@@ -3,7 +3,9 @@ import catSound from '../assets/sounds/cat.mp3';
 export function playCatSound(): void {
 	try {
 		const audio = new Audio(catSound);
-		audio.volume = 0.5;
+		audio.volume = 0.4 + Math.random() * 0.1;
+		audio.preservesPitch = false;
+		audio.playbackRate = 0.85 + Math.random() * 0.4;
 		void audio.play();
 	} catch (error) {
 		console.error("Failed to play cat sound", error);
